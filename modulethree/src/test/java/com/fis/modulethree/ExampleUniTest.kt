58bb5193fd82c3359e018029abdc2e054b7a3c95
Carlos Mauricio Idarraga Espitia <carlos.idarraga@globant.com>
2021-07-19 11:01:10 -0500

    Added module two and three
    Added lane to module two and three

---
+++ modulethree/src/test/java/com/fis/modulethree/ExampleUniTest.kt
@@ -0,0 +1,11 @@
+package com.fis.modulethree
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class ExampleUniTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(5, 5 + 2)
+    }
+}
---
+++ moduletwo/src/main/java/com/fis/moduletwo/MyClass.kt
@@ -0,0 +1,4 @@
+package com.fis.moduletwo
+
+class MyClass {
+}
