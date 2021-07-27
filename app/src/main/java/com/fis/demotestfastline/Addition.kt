d1019b33dac0c840ddbcd42c50c0874647e1367e
Mauricio Idárraga <carlos.idarraga@globant.com>
2021-07-27 09:46:49 -0500

    Added jacoco.gradle
    updated implementation of jacoco

--- app/src/main/java/com/fis/demotestfastline/Addition.kt
+++ app/src/main/java/com/fis/demotestfastline/Addition.kt
@@ -4,4 +4,8 @@ class Addition {
     fun execute(a:Int, b: Int) : Int {
         return a+b
     }
+
+    fun execute2(a:Int, b: Int) : Int {
+        return a+b
+    }
 }
--- app/src/test/java/com/fis/demotestfastline/AdditionTest.kt
+++ app/src/test/java/com/fis/demotestfastline/AdditionTest.kt
@@ -3,13 +3,16 @@ package com.fis.demotestfastline
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+
+class AdditionTest {
     @Test
     fun addition_isCorrect() {
         val addition = Addition()
